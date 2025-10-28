9940dadbf52688b26425b8330dee869978fd894e
Lavish-Pundir <your-new-lavishpundir90@gmail.com>
2025-10-28 10:54:06 +0530

    My first commit

--- config/connect.js
+++ config/connect.js
@@ -7,8 +7,8 @@ const connectDB = async () => {
    await mongoose.connect(process.env.MONGO_URI);  /* , {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    } 
-   */;
+    }; 
+   */
     console.log("✅ MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
