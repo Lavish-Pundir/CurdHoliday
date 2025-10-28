6d9e62b1695847fb058853b6c2dc7739101d517f
Lavish-Pundir <your-new-lavishpundir90@gmail.com>
2025-10-28 11:15:09 +0530

    MY second commit authJWT add comment code

--- middleware/auth.js
+++ middleware/auth.js
@@ -26,26 +26,17 @@ const authJWT = (req, res, next) => {
 export default authJWT;
 
 
+// --- IGNORE ---
 
-// const auth = (req, res, next) => {
-//     try {
-//         const authHeader = req.header('Authorization');
-
-//         if (!authHeader) {
-//             return res.status(401).json({ message: 'No token provided'});
-
-//         }
-//         // Expect format: "Bearer <token>"
-//         const token = authHeader.split(" ")[1];
-//         if !(token) {
-//             return res.status(401).json({ message: " Invalid token format" });
-//         }
-//         const decoded = jwt.verify(token, process.env.JET_SECRET);
-//         req.user = decoded;
-//         next();
-//     } catch (error) {
-//         return res.status(403).json({ message: "Invalid or expired token", error: error.message });
-//     }
+// const authJWT = (req, res, next) => {
+//   try {
+//     const token = req.header("Authorization").replace("Bearer ", "");
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = decoded;
+//     next(); 
+//   } catch (error) {
+//     res.status(401).send({ error: "Please authenticate." });
+//   }   
 // };
 
-// export default auth;
+// export default authJWT;
